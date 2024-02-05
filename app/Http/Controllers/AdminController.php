b568004f6d33b1a0e9aacb70e46edeae8c7032a9
arunvaghela <43928109+arunvaghela@users.noreply.github.com>
2024-02-05 22:28:56 +0530

    add admin theme

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        return view('admin.home');
+    }
+
+    public function form()
+    {
+        return view('admin.form');
+    }
+}
